99aa734f4c11553c2d4648e92b8ff32d3eca72f1
Tautvydas Bujauskas <t.bujauskas@gmail.com>
2023-08-01 17:47:24 +0300

    update

--- infra/03-k8s/argocd.tf
+++ infra/03-k8s/argocd.tf
@@ -1,15 +1,29 @@
 
-module "argocd" {
-  source        = "squareops/argocd/kubernetes"
-  version       = "1.0.0"
-  chart_version = "5.42.1"
-  namespace     = "argocd"
-  argocd_config = {
-    hostname                     = "argocd.tagai.uk"
-    values_yaml                  = file("${path.module}/argocd/values.yaml")
-    redis_ha_enabled             = false
-    autoscaling_enabled          = false
-    slack_notification_token     = "xoxb-qQ8486bluEuvmxrYx"
-    argocd_notifications_enabled = false
-  }
+# module "argocd" {
+#   source        = "squareops/argocd/kubernetes"
+#   version       = "1.0.0"
+#   chart_version = "5.42.1"
+#   namespace     = "argocd"
+#   argocd_config = {
+#     hostname                     = "argocd.tagai.uk"
+#     values_yaml                  = file("./argocd/values.yaml")
+#     redis_ha_enabled             = false
+#     autoscaling_enabled          = false
+#     slack_notification_token     = "xoxb-qQ8486bluEuvmxrYx"
+#     argocd_notifications_enabled = false
+#   }
+# }
+resource "helm_release" "argocd" {
+  name             = "argo-cd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "23.2.0"
+  namespace        = "argo-cd"
+  create_namespace = true
+  values = [
+    file("${path.module}/argocd/values.yaml")
+  ]
+  depends_on = [
+    data.aws_eks_cluster.this
+  ]
 }
