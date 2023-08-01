c5574e747836193e2b04a4595282b2da74e10b95
Tautvydas Bujauskas <t.bujauskas@gmail.com>
2023-08-01 21:20:08 +0300

    update

--- modules/K8s/Traefik/main.tf
+++ modules/K8s/Traefik/main.tf
@@ -5,7 +5,7 @@ data "aws_eks_cluster" "this" {
 resource "helm_release" "traefik" {
   name             = "traefik"
   repository       = "https://traefik.github.io/charts"
-  chart            = "traefik/traefik"
+  chart            = "traefik"
   version          = var.traefik_helm_verion #23.2.0
   namespace        = "traefik"
   create_namespace = true
